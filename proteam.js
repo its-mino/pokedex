7ea1fdfa2e951486ff9ec2f1397d72dee8820950
unknown <nick.p.orr@gmail.com>
2019-04-12 18:49:12 -0400

    Stuff that doesnt work

--- proteam.js
+++ proteam.js
@@ -1,15 +1,18 @@
-targetPokemon = "Torchic"
-
+targetPokemon = "torchic"
 
 window.onload = function()
 {
-  fetch('http://localhost/pokedex/getPokemon.php/?name='+targetPokemo)
-    .then(function(response) {
-    	return response.json();
-    })
-    .then(function(myJson) {
-    	writeImage(myJson[0]['Sprite']);
-  	});
+  function choosePokemon(num)
+  {
+    id = num
+    fetch('http://localhost/pokedex/getPokemon.php/?name='+targetPokemon)
+      .then(function(response) {
+      	return response.json();
+      })
+      .then(function(myJson) {
+      	writeImage(myJson[0]['Sprite']);
+    	});
+  }
 }
 
 function writeImage(link)
