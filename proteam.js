1e4c1b323c96b658eb179b9f0f8a3b1ff9aab412
unknown <nick.p.orr@gmail.com>
2019-04-13 19:26:15 -0400

    Proteams added

--- proteam.js
+++ proteam.js
@@ -13,18 +13,18 @@ window.onload = function()
   .then(function(myJson)
   {
     teams = myJson;
-    console.log(teams);
-
 
     for(var i = 0; i < teams.length; i++)
     {
+      console.log('<tr><h1>' + teams[i]['PlayerName'] + ' - ' + teams[i]['Ranking'] + '</h1></tr>')
       $('#proteamTable').append(
-                  '<tr><td><h3 id="name'+i+'1"></h3><img id="imageHolder'+i+'1"></td>'+
-                  '<td><h3 id="name'+i+'2"></h3><img id="imageHolder'+i+'2"></td>'+
-                  '<td><h3 id="name'+i+'3"></h3><img id="imageHolder'+i+'3"></td>'+
-                  '<td><h3 id="name'+i+'4"></h3><img id="imageHolder'+i+'4"></td>'+
-                  '<td><h3 id="name'+i+'5"></h3><img id="imageHolder'+i+'5"></td>'+
-                  '<td><h3 id="name'+i+'6"></h3><img id="imageHolder'+i+'6"></td></tr>');
+                  '<tr><td colspan = "6"><h1 class = "proteamName">' + teams[i]['PlayerName'] + ' - ' + teams[i]['Ranking'] + '</h1></td></tr>' +
+                  '<tr><td><h3 class = "proteamName" id="name'+i+'1"></h3><img onclick="pokemonDetails(\''+teams[i]['Pokemon1']+'\')" class = "proteamim" id="imageHolder'+i+'1"></td>'+
+                  '<td><h3 class = "proteamName" id="name'+i+'2"></h3><img onclick="pokemonDetails(\''+teams[i]['Pokemon2']+'\')" class = "proteamim" id="imageHolder'+i+'2"></td>'+
+                  '<td><h3 class = "proteamName" id="name'+i+'3"></h3><img onclick="pokemonDetails(\''+teams[i]['Pokemon3']+'\')" class = "proteamim" id="imageHolder'+i+'3"></td>'+
+                  '<td><h3 class = "proteamName" id="name'+i+'4"></h3><img onclick="pokemonDetails(\''+teams[i]['Pokemon4']+'\')" class = "proteamim" id="imageHolder'+i+'4"></td>'+
+                  '<td><h3 class = "proteamName" id="name'+i+'5"></h3><img onclick="pokemonDetails(\''+teams[i]['Pokemon5']+'\')" class = "proteamim" id="imageHolder'+i+'5"></td>'+
+                  '<td><h3 class = "proteamName" id="name'+i+'6"></h3><img onclick="pokemonDetails(\''+teams[i]['Pokemon6']+'\')" class = "proteamim" id="imageHolder'+i+'6"></td></tr>');
 
       for(var j = 1; j<=6; j++)
       {
@@ -37,16 +37,11 @@ window.onload = function()
 
 
 function writeResults(pokemonName,i,j){
-
-  console.log(pokemonName)
-
     fetch('http://localhost/pokedex/getPokemon.php/?name='+pokemonName)
       .then(function(response) {
       	return response.json();
       })
       .then(function(myJson) {
-        console.log(i,j)
-        console.log(myJson)
       	writeImage(myJson[0]['Sprite'],pokemonName,i,j);
     	});
 }
@@ -57,4 +52,9 @@ function writeImage(link,name, i, j)
     $(('#name' + i) + j).append(name)
     $(('#imageHolder' + i) + j).attr('src', link)
 
+}
+
+function pokemonDetails(name)
+{
+	window.location.href = '/pokedex/pokemon.html?name='+name;
 }
