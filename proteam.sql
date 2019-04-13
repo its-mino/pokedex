d4f981f85d11299a6ce472708599c1b68a42e87c
unknown <nick.p.orr@gmail.com>
2019-04-13 18:47:43 -0400

    Updated stuff

--- proteam.sql
+++ proteam.sql
@@ -1,6 +1,6 @@
-Drop table if exists Proteam;
+Drop table if exists proteam;
 
-CREATE TABLE Proteam (
+CREATE TABLE proteam (
 PlayerName varchar(100) NOT NULL,
 Ranking varchar(100) NOT NULL,
 Pokemon1 varchar(100) NOT NULL,
@@ -11,11 +11,11 @@ Pokemon5 varchar(100) NOT NULL,
 Pokemon6 varchar(100) NOT NULL
 );
 
-INSERT INTO Proteam VALUES('Toler Webb' , 'Masters Div Champion', 'landorus', 'amoonguss', 'politoed' , 'aegislash', 'thundurus', 'gardevoir');
-INSERT INTO Proteam VALUES('Raphael Bagara' , 'Masters Div Runner-Up', 'gardevoir', 'amoonguss', 'heatran' , 'scrafty', 'landorus', 'thundurus');
-INSERT INTO Proteam VALUES('Aaron Zheng' , 'Masters Div Semi-Finalist', 'rotom', 'landorus', 'amoonguss' , 'salamence', 'tyranitar', 'aegislash');
-INSERT INTO Proteam VALUES('Blake Hopper' , 'Masters Div Semi-Finalist', 'charizard', 'conkeldurr', 'sylveon' , 'aegislash', 'landorus', 'thundurus');
-INSERT INTO Proteam VALUES('Angel Miranda' , 'Masters Div Top 8', 'aegislash', 'landorus', 'tyranitar' , 'charizard', 'jellicent', 'sylveon');
-INSERT INTO Proteam VALUES('Wolfe Glick' , 'Masters Div Top 8', 'kangaskhan', 'heatran', 'landorus' , 'thundurus', 'amoonguss', 'milotic');
-INSERT INTO Proteam VALUES('Hayden McTavish' , 'Masters Div Top 8', 'rotom', 'salamence', 'heatran' , 'aegislash', 'cresselia', 'conkeldurr');
-INSERT INTO Proteam VALUES('Alex Underhill' , 'Masters Div Top 8', 'excadrill', 'gastrodon', 'cresselia' , 'salamence', 'rotom', 'tyranitar');
+INSERT INTO proteam VALUES('Toler Webb' , 'Masters Div Champion', 'landorus', 'amoonguss', 'politoed' , 'aegislash', 'thundurus', 'gardevoir');
+INSERT INTO proteam VALUES('Raphael Bagara' , 'Masters Div Runner-Up', 'gardevoir', 'amoonguss', 'heatran' , 'scrafty', 'landorus', 'thundurus');
+INSERT INTO proteam VALUES('Aaron Zheng' , 'Masters Div Semi-Finalist', 'rotom', 'landorus', 'amoonguss' , 'salamence', 'tyranitar', 'aegislash');
+INSERT INTO proteam VALUES('Blake Hopper' , 'Masters Div Semi-Finalist', 'charizard', 'conkeldurr', 'sylveon' , 'aegislash', 'landorus', 'thundurus');
+INSERT INTO proteam VALUES('Angel Miranda' , 'Masters Div Top 8', 'aegislash', 'landorus', 'tyranitar' , 'charizard', 'jellicent', 'sylveon');
+INSERT INTO proteam VALUES('Wolfe Glick' , 'Masters Div Top 8', 'kangaskhan', 'heatran', 'landorus' , 'thundurus', 'amoonguss', 'milotic');
+INSERT INTO proteam VALUES('Hayden McTavish' , 'Masters Div Top 8', 'rotom', 'salamence', 'heatran' , 'aegislash', 'cresselia', 'conkeldurr');
+INSERT INTO proteam VALUES('Alex Underhill' , 'Masters Div Top 8', 'excadrill', 'gastrodon', 'cresselia' , 'salamence', 'rotom', 'tyranitar');
